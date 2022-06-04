c5c1fd71877f714eb562514a2979ea7c79105e13
analogpixel <matt.poepping@gmail.com>
2022-06-04 12:34:17 -0600

    bless

---
+++ blessed/demo.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+# https://github.com/jquast/blessed
+
+from blessed import Terminal
+
+term = Terminal()
+
+print(term.home + term.clear + term.move_y(term.height // 2))
+print(term.black_on_darkkhaki(term.center('press any key to continue.')))
+
+with term.cbreak(), term.hidden_cursor():
+    inp = term.inkey()
+
+print(term.move_down(2) + 'You pressed ' + term.bold(repr(inp)))
